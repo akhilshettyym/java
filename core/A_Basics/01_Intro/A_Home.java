77d16121ba4533b223a13fe0431e50fc196460d4
Shetty, Akhil <akhilshettym2003@gmail.com>
2026-05-12 18:16:26 +0530

    Home introduction to java

---
+++ core/A_Basics/01_Intro/A_Home.java
@@ -0,0 +1,12 @@
+public class A_Home {
+    /*
+     * Learn Java
+     * Java is a popular programming language.
+     * 
+     * Java is used to develop mobile apps, web apps, desktop apps, games and much
+     * more.
+     */
+    public static void main(String[] args) {
+        System.out.println("Java is a popular programming language.");
+    }
+}
